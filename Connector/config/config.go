bd107d0f6dd9337d8565aea8e8a1589cc104c413
kuzid-17 <kuzid.24@gamil.com>
2026-02-08 20:26:28 +0300

    [Connector] Add settings structs

--- Connector/config/config.go
+++ Connector/config/config.go
@@ -1,3 +1,22 @@
 package config
 
-// issue-2
+type Config struct {
+	DBSettings      DBSettings      `yaml:"DBSettings"`
+	ProgramSettings ProgramSettings `yaml:"ProgramSettings"`
+}
+
+type ProgramSettings struct {
+	JiraURL           string `yaml:"jiraUrl"`
+	ThreadCount       int    `yaml:"threadCount"`
+	IssueInOneRequest int    `yaml:"issueInOneRequest"`
+	MinTimeSleep      int    `yaml:"minTimeSleep"`
+	MaxTimeSleep      int    `yaml:"maxTimeSleep"`
+}
+
+type DBSettings struct {
+	User     string `yaml:"dbUser"`
+	Password string `yaml:"dbPassword"`
+	Host     string `yaml:"dbHost"`
+	Port     int    `yaml:"dbPort"`
+	Name     string `yaml:"dbName"`
+}
