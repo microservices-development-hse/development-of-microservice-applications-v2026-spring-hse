9810b9d41756808de5842232564d0d939d4a7706
Anfisa Filipova <asfilipova@edu.hse.ru>
2026-02-10 14:02:38 +0300

    [Backend] add config parser and test

---
+++ Backend/internal/config/config.go
@@ -0,0 +1,51 @@
+package config
+
+import (
+	"errors"
+	"os"
+
+	"github.com/go-playground/validator/v10"
+	"gopkg.in/yaml.v3"
+)
+
+var errReadConfig = errors.New("failed to read config file")
+var errParseConfig = errors.New("failed to parse config file")
+
+type Config struct {
+	DBSettings      DBConfig      `yaml:"DBSettings" validate:"required"`
+	ProgramSettings ProgramConfig `yaml:"ProgramSettings" validate:"required"`
+}
+
+type DBConfig struct {
+	DBUser     string `yaml:"dbUser" validate:"required"`
+	DBPassword string `yaml:"dbPassword" validate:"required"`
+	DBHost     string `yaml:"dbHost" validate:"required,hostname_rfc1123|ip"`
+	DBPort     int    `yaml:"dbPort" validate:"required,gt=0,lte=65535"`
+	DBName     string `yaml:"dbName" validate:"required"`
+}
+
+type ProgramConfig struct {
+	BindAddress      string `yaml:"bindAddress" validate:"required,ip"`
+	BindPort         int    `yaml:"bindPort" validate:"required,gt=0,lte=65535"`
+	ResourceTimeout  int    `yaml:"resourceTimeout" validate:"required,gt=0"`
+	AnalyticsTimeout int    `yaml:"analyticsTimeout" validate:"required,gt=0"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, errReadConfig
+	}
+
+	var config Config
+	if err := yaml.Unmarshal(data, &config); err != nil {
+		return nil, errParseConfig
+	}
+
+	validate := validator.New()
+	if err := validate.Struct(config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
