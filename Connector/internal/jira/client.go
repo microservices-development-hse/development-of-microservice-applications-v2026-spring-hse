a0fd880046f897647151698699d1c3d1ab83a393
kuzid-17 <kuzid.24@gamil.com>
2026-02-22 15:10:37 +0300

    [Connector] add jira models & http client

--- Connector/internal/jira/client.go
+++ Connector/internal/jira/client.go
@@ -1,3 +1,25 @@
-// issue-6	HTTP клиент с net/http
-
 package jira
+
+import (
+	"net/http"
+	"time"
+)
+
+type Client struct {
+	baseURL    string
+	httpClient *http.Client
+}
+
+func NewClient(baseURL string) *Client {
+	return &Client{
+		baseURL: baseURL,
+		httpClient: &http.Client{
+			Timeout: 30 * time.Second,
+			Transport: &http.Transport{
+				MaxIdleConns:        100,
+				MaxIdleConnsPerHost: 100,
+				IdleConnTimeout:     90 * time.Second,
+			},
+		},
+	}
+}
--- Connector/internal/models/jira/issue.go
+++ Connector/internal/models/jira/issue.go
@@ -1,3 +1,54 @@
-// issue-6
-
 package jira
+
+import "time"
+
+type IssueSearchResponse struct {
+	StartAt    int     `json:"startAt"`
+	MaxResults int     `json:"maxResults"`
+	Total      int     `json:"total"`
+	Issues     []Issue `json:"issues"`
+}
+
+type Issue struct {
+	ID        string     `json:"id"`
+	Key       string     `json:"key"`
+	Self      string     `json:"self"`
+	Fields    Fields     `json:"fields"`
+	Changelog *Changelog `json:"changelog,omitempty"`
+}
+
+type Fields struct {
+	Summary string    `json:"summary"`
+	Status  Status    `json:"status"`
+	Created time.Time `json:"created"`
+	Updated time.Time `json:"updated"`
+}
+
+type Status struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type Changelog struct {
+	Total     int       `json:"total"`
+	Histories []History `json:"histories"`
+}
+
+type History struct {
+	ID      string    `json:"id"`
+	Author  Author    `json:"author"`
+	Created time.Time `json:"created"`
+	Items   []Item    `json:"items"`
+}
+
+type Author struct {
+	Self        string `json:"self"`
+	Name        string `json:"name"`
+	DisplayName string `json:"displayName"`
+}
+
+type Item struct {
+	Field string `json:"field"`
+	From  string `json:"fromString"`
+	To    string `json:"toString"`
+}
