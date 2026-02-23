099a8fbcfc5406f46dee2f71df4eb271dca3f176
kuzid-17 <kuzid.24@gamil.com>
2026-02-23 17:34:59 +0300

    [Connector] add retry mechanism

--- Connector/internal/jira/retry.go
+++ Connector/internal/jira/retry.go
@@ -1,3 +1,31 @@
-// issue-8	Механизм exponential backoff
-
 package jira
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/microservices-development-hse/connector/internal/logger"
+)
+
+type RetryConfig struct {
+	MinTimeSleep int
+	MaxTimeSleep int
+}
+
+func WithRetry(cfg RetryConfig, fn func() error) error {
+	var err error
+
+	for currentSleep := cfg.MinTimeSleep; currentSleep <= cfg.MaxTimeSleep; currentSleep *= 2 {
+		if err = fn(); err == nil {
+			return nil
+		}
+
+		logger.Warning(fmt.Sprintf("Jira request failed, retrying in %dms: %v", currentSleep, err))
+		time.Sleep(time.Duration(currentSleep) * time.Millisecond)
+	}
+
+	err = fmt.Errorf("retry limit exceeded (maxSleep=%dms): %w", cfg.MaxTimeSleep, err)
+	logger.Error(err.Error())
+
+	return err
+}
